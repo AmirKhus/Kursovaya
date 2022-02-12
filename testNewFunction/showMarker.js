85a4ec234665ce9bfaec8c91848fb613ff1cf130
AmirKhus <70978948+AmirKhus@users.noreply.github.com>
2022-02-13 01:11:57 +0300

    Добавлена кнопка для филтрации

--- testNewFunction/showMarker.js
+++ testNewFunction/showMarker.js
@@ -12,6 +12,12 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     subdomains: ['a', 'b', 'c'],
 }).addTo(map)
 
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}',
+    {    attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        subdomains: ['a', 'b', 'c'],
+    }).addTo(map);
+
 var myURL = jQuery('script[src$="showMarker.js"]')
     .attr('src')
     .replace('showMarker.js', '')
@@ -33,13 +39,29 @@ var musorIcon = L.icon({
     popupAnchor: [0, -14],
 })
 
+var filterIcon = L.icon({
+    iconUrl: myURL + '../maps/images/filter.png',
+    iconRetinaUrl: myURL + '../maps/images/filter.png',
+    iconSize: [29, 24],
+    iconAnchor: [9, 21],
+    popupAnchor: [0, -14],
+})
+
 
 var markerClusters = L.markerClusterGroup()
 
+let arrVidMusora = [];
+
 for (var i = 0; i < markers.length; ++i) {
+    //заполнение массива с видами мусора
+    if (arrVidMusora.indexOf(markers[i].typeOfEnvironmentalProblem) === -1)
+        arrVidMusora.push(markers[i].typeOfEnvironmentalProblem)
+
     if (markers[i].id > 0) {
         var popup =
             markers[i].name +
+            '<br/>' +
+            markers[i].typeOfEnvironmentalProblem +
             '<br/>' +
             markers[i].city +
             '<br/><b>IATA/FAA:</b> ' +
@@ -51,19 +73,22 @@ for (var i = 0; i < markers.length; ++i) {
             ' m' +
             '<br/><b>Timezone:</b> ' +
             markers[i].tz
+
         if (markers[i].id%2 === 0 ) {
             var m = L.marker([markers[i].lat, markers[i].lng], {
                 icon: musorIcon,
+                tags: [markers[i].typeOfEnvironmentalProblem],
             }).bindPopup(popup,
-                {removable: true, editable: true, maxWidth: 800, autoPan: false}
+                {removable: true, editable: true,tags: ['fast']}
             );
 
             markerClusters.addLayer(m)
         }else {
             var m = L.marker([markers[i].lat, markers[i].lng], {
                 icon: myIcon,
+                tags: [markers[i].typeOfEnvironmentalProblem],
             }).bindPopup(popup,
-                {removable: true, editable: true, maxWidth: 800, autoPan: false}
+                {removable: true, editable: true,tags: ['fast']}
             );
 
             markerClusters.addLayer(m)
@@ -72,3 +97,10 @@ for (var i = 0; i < markers.length; ++i) {
 }
 
 map.addLayer(markerClusters)
+
+// функция кнопки с фильрацией
+L.control.tagFilterButton({
+    data: arrVidMusora,
+    icon: '<img src = "../maps/images/filter.png">',
+    filterOnEveryClick: true
+}).addTo(map);
